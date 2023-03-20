6e1a2b6d0ad9c455e8672d288a34a1075956d253
unknown <silvateranma@gmail.com>
2023-03-20 13:13:17 +0100

    variable PORT added

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
+// Middlewares
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
@@ -106,7 +107,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
